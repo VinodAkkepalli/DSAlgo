72c06ca0746c8555d5bca18cd3a5e70d6bd195ee
Vinod Akkepalli <shining.vinod@gmail.com>
2026-02-19 00:09:30 +0530

    Group Anagrams into sublists

--- src/interviews/AnagramGrouping.java
+++ src/interviews/AnagramGrouping.java
@@ -4,10 +4,24 @@ import java.util.*;
 
 /**
  * <b>Description</b> : Given a list of inputs, print out the list of anagrams
+ *
+ * <b>Naive Solution</b>
  *  1) convert each string to char array and sort char array
  *  2) create a string with sorted char array
  *  3) create HashMap with sorted string as key, and original string as value
  *  4) Print HashMap contents
+ *
+ *  <b>Better Solution</b>
+ *
+ *  1) Create a hash map where each key is a 26-length tuple representing character frequencies,
+ *      and each value is a list of strings belonging to that anagram group.
+ *
+ *  2) For each string in the input:
+ *      Initialize a count array of size 26 with all zeros.
+ *      For each character c in the string, increment the count at the corresponding index.
+ *      Convert the count array to a tuple and use it as the key.
+ *      Append the string to the list associated with this key.
+ *      After processing all strings, return all the lists stored in the hash map.
  *
  * @author Vinod Akkepalli
  */
@@ -19,10 +33,12 @@ public class AnagramGrouping {
 //                "cat", "dog", "loop", "act", "pool", "ogd", "city", "god"));
         List<String> strList = List.of("cat", "dog", "loop", "act", "pool", "ogd", "city", "god");
         groupAnagrams(strList);
+        groupAnagrams2(strList);
     }
 
 
 
+    /** Naive Solution*/
     public static void groupAnagrams(List<String> strList) {
         Map<String, List<String>> hm = new HashMap<>();
         String sortedStr;
@@ -44,9 +60,27 @@ public class AnagramGrouping {
         }
 
         System.out.println("Anagram groups are: ");
-        for(Map.Entry<String, List<String>> entry: hm.entrySet()) {
-            System.out.print(entry.getValue());
+        System.out.println(hm.values());
+    }
+
+    /**Better Solution*/
+    public static void groupAnagrams2(List<String> strList) {
+
+        Map<String, List<String>> res = new HashMap<>();
+
+        for(String s: strList) {
+            int[] charCount = new int[26];
+            for(char ch : s.toCharArray()) {
+                charCount[ch - 'a']++;
+            }
+
+            String str = Arrays.toString(charCount);
+//            System.out.println(str);
+            res.putIfAbsent(str, new ArrayList<>());
+            res.get(str).add(s);
         }
 
+        System.out.println("Anagram groups are: ");
+        System.out.println(res.values());
     }
 }
